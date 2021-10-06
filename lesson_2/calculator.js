f331f26a1357ec36d1953c1fe5960aabe04e6f52
Grace Lim <gracelim@qseudo.local>
2021-10-06 13:32:52 -0400

    Installs readline sync and addition feature to calculator.js.

---
+++ lesson_2/calculator.js
@@ -0,0 +1,26 @@
+const rlSync = require('readline-sync');
+
+console.log("Welcome to Calculator!");
+
+// Ask the user for the first number.
+console.log("What's the first number?");
+let number1 = rlSync.question();
+
+// Ask the user for the second number.
+console.log("What's the second number?");
+let number2 = rlSync.question();
+
+// Ask the user for the operation to perform.
+console.log("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide")
+let operation = rlSync.question();
+
+let output;
+
+if (operation === '1') {
+  output = Number(number1) + Number(number2);
+}
+
+console.log(`The result is ${output}.`);
+
+// Perform the operation on the two numbers.
+// Print the result to the ter  minal.
