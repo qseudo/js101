ae4c83876e54f32321d596bca6c1f31e8fdd7ad8
Grace Lim <gracelim@qseudo.local>
2021-10-12 18:19:18 -0400

    Adds language features to calculator.

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -2,38 +2,45 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function messages(message, lang) {
+  return MESSAGES[lang][message];
+}
+
 function invalidNumber(number) {
   return number.trimStart() === '' || Number.isNaN(Number(number));
 }
 
 const rlSync = require('readline-sync');
+const MESSAGES = require('./calculator_messages.json');
+const LANGUAGE = 'en';
 
 prompt("Welcome to Calculator!");
 
+
 while (true) {
   // Ask the user for the first number.
-  prompt("What's the first number?");
+  prompt(messages('num1', LANGUAGE));
   let number1 = rlSync.question();
 
   while (invalidNumber(number1)) {
-    prompt("Hmm, that doesn't look like a valid number.");
+    prompt(messages('invalidNum', LANGUAGE));
     number1 = rlSync.question();
   }
   // Ask the user for the second number.
-  prompt("What's the second number?");
+  prompt(messages('num2', LANGUAGE));
   let number2 = rlSync.question();
 
   while (invalidNumber(number2)) {
-    prompt("Hmm, that doesn't look like a valid number.");
+    prompt(messages('invalidNum', LANGUAGE));
     number2 = rlSync.question();
   }
 
   // Ask the user for the operation to perform.
-  prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+  prompt(messages('selectOp', LANGUAGE));
   let operation = rlSync.question();
 
   while (!['1', '2', '3', '4'].includes(operation)) {
-    prompt('Must choose 1, 2, 3 or 4');
+    prompt(messages('invalidOp', LANGUAGE));
     operation = rlSync.question();
   }
 
@@ -53,9 +60,9 @@ while (true) {
       break;
   }
 
-  prompt(`The result is ${output}.`);
+  prompt(messages('result', LANGUAGE) + `${output}.`);
 
-  prompt(`Do you want to run another calculation? Y / N`);
+  prompt(messages('repeat', LANGUAGE));
 
   let answer = rlSync.question();
 
