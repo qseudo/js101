462756a293924ddcf276a81d7f49afdd36e54d82
Grace Lim <gracelim@qseudo.local>
2021-10-12 17:39:20 -0400

    Adds first bonus feature of calculator.

--- lesson_2/calculator.js
+++ lesson_2/calculator.js
@@ -10,49 +10,55 @@ const rlSync = require('readline-sync');
 
 prompt("Welcome to Calculator!");
 
-// Ask the user for the first number.
-prompt("What's the first number?");
-let number1 = rlSync.question();
+while (true) {
+  // Ask the user for the first number.
+  prompt("What's the first number?");
+  let number1 = rlSync.question();
 
-while (invalidNumber(number1)) {
-  prompt("Hmm, that doesn't look like a valid number.");
-  number1 = rlSync.question();
+  while (invalidNumber(number1)) {
+    prompt("Hmm, that doesn't look like a valid number.");
+    number1 = rlSync.question();
+  }
+  // Ask the user for the second number.
+  prompt("What's the second number?");
+  let number2 = rlSync.question();
+
+  while (invalidNumber(number2)) {
+    prompt("Hmm, that doesn't look like a valid number.");
+    number2 = rlSync.question();
+  }
+
+  // Ask the user for the operation to perform.
+  prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
+  let operation = rlSync.question();
+
+  while (!['1', '2', '3', '4'].includes(operation)) {
+    prompt('Must choose 1, 2, 3 or 4');
+    operation = rlSync.question();
+  }
+
+  let output;
+  switch (operation) {
+    case '1':
+      output = Number(number1) + Number(number2);
+      break;
+    case '2':
+      output = Number(number1) - Number(number2);
+      break;
+    case '3':
+      output = Number(number1) * Number(number2);
+      break;
+    case '4':
+      output = Number(number1) / Number(number2);
+      break;
+  }
+
+  prompt(`The result is ${output}.`);
+
+  prompt(`Do you want to run another calculation? Y / N`);
+
+  let answer = rlSync.question();
+
+  if (answer[0].toLowerCase() !== 'y') break;
+
 }
-// Ask the user for the second number.
-prompt("What's the second number?");
-let number2 = rlSync.question();
-
-while (invalidNumber(number2)) {
-  prompt("Hmm, that doesn't look like a valid number.");
-  number2 = rlSync.question();
-}
-
-// Ask the user for the operation to perform.
-prompt("What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide");
-let operation = rlSync.question();
-
-while (!['1', '2', '3', '4'].includes(operation)) {
-  prompt('Must choose 1, 2, 3 or 4');
-  operation = rlSync.question();
-}
-
-let output;
-switch (operation) {
-  case '1':
-    output = Number(number1) + Number(number2);
-    break;
-  case '2':
-    output = Number(number1) - Number(number2);
-    break;
-  case '3':
-    output = Number(number1) * Number(number2);
-    break;
-  case '4':
-    output = Number(number1) / Number(number2);
-    break;
-}
-
-prompt(`The result is ${output}.`);
-
-// Perform the operation on the two numbers.
-// Print the result to the ter  minal.
